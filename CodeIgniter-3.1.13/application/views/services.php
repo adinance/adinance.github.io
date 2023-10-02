1b0da3f22e797abc4e7c5766f78d58d37bf7a6b9
Sakdinan-cmogroup <83816802+Sakdinan-cmogroup@users.noreply.github.com>
2023-10-02 08:50:39 +0700

    commit 2-10-66

--- CodeIgniter-3.1.13/application/views/services.php
+++ CodeIgniter-3.1.13/application/views/services.php
@@ -1,57 +1,159 @@
 <section class="servicespala">
 
-<h1>OUR SERVICES</h1>
+    <h1>OUR SERVICES</h1>
 
-<div class="servicescont">
-
-
-
-
-<div class="services_1row">
-
-<div class="services_box">
-  <p><img src="<?php echo BASE_URL; ?>assets/images/news/IMG-New-1.jpg">
-    <br><br>
-    <span>
-      CMO ปรับโครงสร้างธุรกิจสู่ Experience Tech Creator
-      เป้าสิ้นปีแตะ 2 พันล้าน
-    </span>
-    <br><br><a href="#">Read More</a>
-  </p>
-</div>
-
-
-<div class="services_box">
-  <p><img src="<?php echo BASE_URL; ?>assets/images/news/IMG-New-2.jpg">
-    <br><br>
-    <span >
-      เก็บตกภาพบรรยากาศสุดน่ารัก “ทิฟฟานี่ ยัง”
-      จัดเต็มแฟนมีตติ้งส่งมอบความสุขเพื่อยองวอนไทย
-    </span>
-    <br><br><a href="#" >Read More</a>
-  </p>
-</div>
-
-<div class="services_box">
-  <p><img src="<?php echo BASE_URL; ?>assets/images/news/IMG-New-3.jpg">
-    <br><br>
-    <span>
-      K-Pop superstars GOT7 ready to entertain Thai fans via live stream
-    </span>
-    <br><br><a href="#">Read More</a>
-  </p>
-</div>
+    <div class="servicescont">
 
 
 
+<div>
+        <div class="services_1row">
 
-</div>
+            <div class="services_box">
+                <p><img src="<?php echo BASE_URL; ?>assets/images/services/Img-6-1.png">
+                    <br>
+                    <h3 style="color:#00c8fa ;font-weight:600;">EVENTS</h3>
+                    <h5 style="font-weight: 600; padding-top:5%;">
+                    Let's Enjoy<br>Creative Experiences
+                    </h5>
+                    <a style="font-size: 13px;">WE are the leader in Event Management Business integrated with marketing communication and technology to create new creative experiences for clients and consumers of digital era.</a>
+                    <div class="container" style="height:100px ; padding-top:5%;font-size: 10px;">
+                    <div class="services_columns"style="text-align: left;">
+                        <li>Brand activation</li>
+                        <li>Conference & seminar</li>
+                        <li>Exhibitions</li>
+                        <li>International festivals</li>
+                    </div>
+                    <div class="columns" style="text-align: left;">
+                        <li>Mice & pco</li>
+                        <li>Private lifestyle</li>
+                        <li>Public events</li>
+                    </div>
+                    </div>
+
+                </p>
+            </div>
+
+
+            <div class="services_box">
+                <p><img src="<?php echo BASE_URL; ?>assets/images/services/Img-6-2.png">
+                <br>
+                    <h3 style="color:#00c8fa ;font-weight:600;">EQUIPMENTS</h3>
+                    <h5 style="font-weight: 600; padding-top:5%;">
+                    Word Class Production <br>Audio Visual & Lighting Experiences 
+                    </h5>
+                    <a style="font-size: 13px;">We are the leader providing comprehensive services for visual system, lighting system, and audio system with special techniques to respond to the requirements of every form of shows and events with international working standard.</a>
+                    <div class="container" style="height:100px ; padding-top:5%;font-size: 10px;">
+                    <div class="services_columns"style="text-align: left;">
+                        <li>Audio System</li>
+                        <li>Visual System</li>
+                        <li>Lighting System</li>
+                    </div>
+                    <div class="columns" style="text-align: left;">
+                        <li>Special Effect </li>
+                        <li>Event Rental Service</li>
+                        <li>Rigging & structure</li>
+                    </div>
+                    </div>
+            </div>
+
+            <div class="services_box">
+                <p><img src="<?php echo BASE_URL; ?>assets/images/services/Img-6-3.png">
+                <br>
+                    <h3 style="color:#00c8fa ;font-weight:600;">ENTERTAINMENT</h3>
+                    <h5 style="font-weight: 600; padding-top:5%;">
+                    INNOVATIVE GLOBAL <br>PHENOMENON
+                    </h5>
+                    <a style="font-size: 13px;">We are the leader of In-bound Live Entertainment Experience, fully stepping into the Entertainment Business. To entertain Asia and Global and bring world festivals into Thailand.</a>
+                    <div class="container" style="height:100px ; padding-top:5%;font-size: 10px;">
+                    <div class="services_columns"style="text-align: left;">
+                        <li>Concert</li>
+                        <li>Music Festival</li>
+                    </div>
+                    <div class="columns" style="text-align: left;">
+                        <li>Original Entertainment Content</li>
+                        <li>Live Entertainment</li>
+                    </div>
+                    </div>
+            </div>
+
+
+
+        </div>
+
+
+
+        <div class="services_2row">
+
+            <div class="services_box">
+                <p><img src="<?php echo BASE_URL; ?>assets/images/services/Img-6-4.png">
+                <br>
+                    <h3 style="color:#00c8fa ;font-weight:600;">TECHNOLOGY</h3>
+                    <a style="font-size: 13px;">Digital and Technology Solutions is a new business of the company.  We use digital technology to create new dimension of event management and new experience to customers.</a>
+                    <div class="container" style="height:100px ; padding-top:5%;font-size: 10px;">
+                    <div class="services_columns"style="text-align: left;">
+                        <li>Digital marketing</li>
+                        <li>Digital transformation</li>
+                        <li>Web 3 agency</li>
+                        <li>Virtual experience</li>
+                    </div>
+                    <div class="columns" style="text-align: left;">
+                        <li>Generative artificial intelligence (ai)</li>
+                        <li>Augmented and virtual reality</li>
+                        <li>Immersive events</li>
+                        <li>Entertainment tech platform</li>
+                    </div>
+                    </div>
+            </div>
+
+
+            <div class="services_box">
+                <p><img src="<?php echo BASE_URL; ?>assets/images/services/Img-6-5.png">
+                <br>
+                    <h3 style="color:#00c8fa ;font-weight:600;">MUSEUM CREATOR</h3>
+                    <h5 style="font-weight: 600; padding-top:5%;">
+                    360° Innovation <br>Knowledge Experience.
+                    </h5>
+                    <a style="font-size: 13px;">We are the leader in utilizing the modern technology merging with the museum experiences </a>
+                    <div class="container" style="height:100px ; padding-top:5%;font-size: 10px;text-align: left;">
+                        <li>Museum exhibition planning</li>
+                        <li>Construction design</li>
+                        <li>Renovation and management</li>
+                    </div>
+            </div>
+
+            <div class="services_box">
+                <p><img src="<?php echo BASE_URL; ?>assets/images/services/Img-6-6.png">
+                <br>
+                    <h3 style="color:#00c8fa ;font-weight:600;">SPACE FOR RENT </h3>
+                    <h5 style="font-weight: 600; padding-top:5%;">
+                    Play With Creative Space 
+                    </h5>
+                    <a style="font-size: 13px;">We offer rental services for our space. It's a space that combines creativity, art, and offices. Within the project there are 3 large office buildings with studios of various sizes and complete functionality.</a>
+                    <div class="container" style="height:100px ; padding-top:5%;font-size: 10px;">
+                    <div class="services_columns"style="text-align: left;">
+                        <li>Cmo building</li>
+                        <li>Pm center building</li>
+                    </div>
+                    <div class="columns" style="text-align: left;">
+                        <li>Nerve creative building</li>
+                        <li>3 studios</li>
+                    </div>
+                    </div>
+            </div>
 
 
 
 
 
-</div>
+        </div>
+
+
+
+
+
+
+    </div>
 
 
 
