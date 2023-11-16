e941b156e7279d84b2a8a9ff396b5f65dc3a35ae
Sakdinan-cmogroup <83816802+Sakdinan-cmogroup@users.noreply.github.com>
2023-11-16 16:35:19 +0700

    16.35

--- demo/application/views/about_us.php
+++ demo/application/views/about_us.php
@@ -1,13 +1,12 @@
-<section class="cid-tMkgOwYZIa mbr-fullscreen mbr-parallax-background" id="header2-4">
-    <div class="container align-center">
-      <div class="row justify-content-md-center" style="padding-left: 18%;padding-right:18% ;">
-        <div class="mbr-white col-md-10">
-          <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1"
-            style="text-shadow: 2px 2px 4px #000000">
+<section class="about_uspala" >
+    <div class="about_uscont">
+      
+        
+          <h1>
             WHO WE ARE
           </h1>
 
-          <p class="mbr-text align-left pb-3 mbr-fonts-style display-7" style="text-shadow: 2px 2px 4px #000000">
+          <p>
             <a style="color:#00c8fa ;">CMO GROUP </a>is a public listed company established in 1986. The
             company explores all aspect of creative experience covering event
             management , marketing communications, entertainment services,
@@ -15,7 +14,7 @@
             production services such as Audio, Visual & Lighting system,
             video, multimedia and interactive media.
           </p>
-          <p class="mbr-text align-left pb-3 mbr-fonts-style display-7" style="text-shadow: 2px 2px 4px #000000">
+          <p>
             Recently, The company has also repositioned themselves as an
             ‘EXPERIENCE-TECH CREATOR’. By acquiring tech partners that can
             accelerate CMO’s tech capability. CMO aims to become a total
@@ -23,15 +22,14 @@
             through their creativity and technology.
           </p>
 
-          <p class="mbr-text align-left pb-3 mbr-fonts-style display-7" style="text-shadow: 2px 2px 4px #000000">
+          <p>
             Our experience business pillars are:
           </p>
-          <p class="mbr-text align-left pb-3 mbr-fonts-style display-7"
-            style="color:#00c8fa ; text-shadow: 2px 2px 4px #000000">
+          <p style="color:#00c8fa ; text-shadow: 2px 2px 4px #000000">
             ● Events ● Equipments ● Entertainment ● Technology ● Museum
             Creator
           </p>
-        </div>
-      </div>
+     
+      
     </div>
   </section>
--- demo/application/views/parallax_intro.php
+++ demo/application/views/parallax_intro.php
@@ -1,21 +1,14 @@
-  <section class="parallax-intro-bg mbr-fullscreen mbr-parallax-background" id="header1-i">
-    <div class="container">
-      <div class="row justify-content-md-center">
-        <div class="mbr-white col-md-10">
-          <h4 class="display-2" style="text-align: center; text-shadow: 2px 2px 4px #000000; font-size: 44px; ">
-            WORLD CLASS<br>
-            EXPERIENCE-TECH CREATOR
-          </h4>
-          <!-- <h4 class="display-2" style="
-                text-align: center;
-                padding-top: 20%;
-                color: #0083c9;
-                text-shadow: 2px 2px 4px #000000;
-              ">
-            EXPERIENCE-TECH CREATOR
-          </h4> -->
-        </div>
-      </div>
-    </div>
-  </section>
+  <section>
 
+
+    <div class="parallax-intropala">
+      <h4>
+        WORLD CLASS<br>
+        EXPERIENCE-TECH CREATOR
+      </h4>
+    </div>
+
+
+
+
+  </section>
