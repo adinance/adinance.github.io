7f72647296801bd23c10f304a32fefc9d6cacd13
Sakdinan-cmogroup <83816802+Sakdinan-cmogroup@users.noreply.github.com>
2023-11-20 17:47:20 +0700

    Update navbar3(pd).php

--- demo/application/views/navbar3(pd).php
+++ demo/application/views/navbar3(pd).php
@@ -1,15 +1,11 @@
-<section>
 
 
+  <!-- for localhost start -->
 
-  <!-- for prductsion start -->
+
+  <section>
 
   <div class="main-wrapper">
-
-
-
-
-
     <nav class="navbar3">
 
 
@@ -17,123 +13,130 @@
 
 
 
+      <!-- <div class="LOGO">
+          <a href="http://localhost/adinance.github.io/demo/index.php/Home"><img src="<?php echo BASE_URL; ?>assets/images/cmo-white-164x62.png" /></a>
+          <button type="button" class="navbar3-toggler">
+            <i class="fas fa-bars"></i>
+          </button>
+        </div> -->
+
+        <div class = "LOGO">
+          <a href="<?php echo BASE_URL;?>index.php/Home" class = "navbar-brand"><img src="<?php echo BASE_URL; ?>assets/images/cmo-white-164x62.png" /></a>
+          <button type = "hambtn" class = "navbar3-toggler">
+            <i class = "fas fa-bars"></i>
+          </button>
+        </div>
+
+
+
+        <div class="navbar3-collapse">
+          <ul class="navbar3-nav">
 
 
 
 
 
 
-      <div class="brand-and-icon">
-        <a href="<?php echo BASE_URL;?>index.php/Home"><img src="<?php echo BASE_URL; ?>assets/images/cmo-white-164x62.png" /></a>
-        <button type="button" class="navbar3-toggler">
-          <i class="fas fa-bars"></i>
-        </button>
-      </div>
 
-      <div class="navbar3-collapse">
-        <ul class="navbar3-nav">
-          <li>
-            <a href="#" class="menu-link">
-              ABOUT
-              <span class="drop-icon">
-                <i class="fas fa-chevron-down"></i>
-              </span>
-            </a>
-            <div class="sub-menu w3-animate-opacity">
-              <!-- item -->
-              <div class="sub-menu-item"><br>
-                <!-- <h4>top categories</h4> -->
-                <ul>
-                  <li><a href="<?php echo BASE_URL;?>index.php/About" style="font-weight: bold;">Who We Are</a></li>
-                  <li><a href="#" style="font-weight: bold;">Vision/Mission and Core Value</a></li>
-                  <li><a href="#" style="font-weight: bold;">History and Milestones</a></li>
-                  <li><a href="#" style="font-weight: bold;">Business Overview</a></li>
+            <li>
+              <a href="#" class="menu-link">
+                ABOUT
+                <span class="drop-icon">
+                  <i class="fas fa-chevron-down"></i>
+                </span>
+              </a>
+              <div class="sub-menu w3-animate-opacity">
+                <!-- item -->
+                <div class="sub-menu-item"><br>
+                  <!-- <h4>top categories</h4> -->
+                  <ul>
+                    <li><a href="<?php echo BASE_URL;?>index.php/About" style="font-weight: bold;">Who We Are</a></li>
+                    <li><a href="#" style="font-weight: bold;">Vision/Mission and Core Value</a></li>
+                    <li><a href="#" style="font-weight: bold;">History and Milestones</a></li>
+                    <li><a href="#" style="font-weight: bold;">Business Overview</a></li>
 
-                </ul>
+                  </ul>
+                </div>
+                <!-- end of item -->
+                <!-- item -->
+                <div class="sub-menu-item"><br>
+                  <!-- <h4>top categories</h4> -->
+                  <ul>
+                    <li><a href="#" style="font-weight: bold;">Company Structure</a></li>
+                    <li><a href="#" style="font-weight: bold;">Board of Directors</a></li>
+                    <li><a href="#" style="font-size: 0.5rem important!;">- Message frome the Chairman</a></li>
+                    <li><a href="#" style="font-size: 0.5rem important!;">- Board of Directors</a></li>
+                    <li><a href="#" style="font-size: 0.5rem important!;">- Sub Committees</a></li>
+                  </ul>
+                </div>
+                <!-- end of item -->
+                <!-- item -->
+                <div class="sub-menu-item" style="margin: 0%;"><br>
+                  <ul>
+                    <li><a href="#" style="font-weight: bold;">Management Team</a></li>
+                    <li><a href="#" style="font-weight: bold;">Organizaion Structure</a></li>
+                    <li><a href="#" style="font-weight: bold;">Important Informaion On the</a></li>
+                    <li><a href="#" style="font-weight: bold;">Company's Employees</a></li>
+                    <li><a href="#" style="font-weight: bold;">Award</a></li>
+                  </ul>
+                </div>
+                <!-- end of item -->
+                <!-- item -->
+                <div class="sub-menu-item">
+                  <img src="<?php echo BASE_URL; ?>assets/images/about-subnav.png" />
+                </div>
+                <!-- end of item -->
               </div>
-              <!-- end of item -->
-              <!-- item -->
-              <div class="sub-menu-item"><br>
-                <!-- <h4>top categories</h4> -->
-                <ul>
-                  <li><a href="#" style="font-weight: bold;">Company Structure</a></li>
-                  <li><a href="#" style="font-weight: bold;">Board of Directors</a></li>
-                  <li><a href="#" style="font-size: 0.5rem important!;">- Message frome the Chairman</a></li>
-                  <li><a href="#" style="font-size: 0.5rem important!;">- Board of Directors</a></li>
-                  <li><a href="#" style="font-size: 0.5rem important!;">- Sub Committees</a></li>
-                </ul>
-              </div>
-              <!-- end of item -->
-              <!-- item -->
-              <div class="sub-menu-item" style="margin: 0%;"><br>
-                <ul>
-                  <li><a href="#" style="font-weight: bold;">Management Team</a></li>
-                  <li><a href="#" style="font-weight: bold;">Organizaion Structure</a></li>
-                  <li><a href="#" style="font-weight: bold;">Important Informaion On the</a></li>
-                  <li><a href="#" style="font-weight: bold;">Company's Employees</a></li>
-                  <li><a href="#" style="font-weight: bold;">Award</a></li>
-                </ul>
-              </div>
-              <!-- end of item -->
-              <!-- item -->
-              <div class="sub-menu-item">
-                <img src="<?php echo BASE_URL; ?>assets/images/about-subnav.png" />
-              </div>
-              <!-- end of item -->
-            </div>
-          </li>
+            </li>
 
-          <li>
-            <a href="<?php echo BASE_URL;?>index.php/Services">SERVICES</a>
-          </li>
-          <li>
-            <a href="<?php echo BASE_URL;?>index.php/Work">WORKS</a>
-          </li>
-          <li>
-            <a href="<?php echo BASE_URL;?>index.php/News">NEWS</a>
-          </li>
-          <li>
-            <a href="https://cmo-group.com/ir/index.php?lang=en&menu=home" target="_blank">INVESTORS</a>
-          </li>
-          <li>
-            <a href="https://cmo-group.com/creative-play-ground.html" target="_blank">SPACE RENTAL</a>
-          </li>
-          <li>
-            <a href="https://career.cmo-group.com/" target="_blank">CAREERS</a>
-          </li>
-          <li>
-            <a href="#footer2-l">CONTACT US</a>
-          </li>
+            <li>
+              <a href="<?php echo BASE_URL;?>index.php/Services">SERVICES</a>
+            </li>
+            <li>
+              <a href="<?php echo BASE_URL;?>index.php/Work">WORKS</a>
+            </li>
+            <li>
+              <a href="<?php echo BASE_URL;?>index.php/News">NEWS</a>
+            </li>
+            <li>
+              <a href="https://cmo-group.com/ir/index.php?lang=en&menu=home" target="_blank">INVESTORS</a>
+            </li>
+            <li>
+              <a href="https://cmo-group.com/creative-play-ground.html" target="_blank">SPACE RENTAL</a>
+            </li>
+            <li>
+              <a href="https://career.cmo-group.com/" target="_blank">CAREERS</a>
+            </li>
+            <li>
+              <a href="#contract">CONTACT US</a>
+            </li>
+            <li>
+              <a href="#">TH</a>
+            </li>
 
-        </ul>
-      </div>
-      <div class="brand-and-icon"><a style="color:#fff;font-size: 12px !important; ">TH</a></div>
+          </ul>
+
+
+
+
+        </div>
 
 
 
 
 
 
+     
     </nav>
-
-
-
   </div>
 
-  <!-- for prductsion end -->
-
-
-
-
-
-
-
-
-
-
+  <!-- for localhost end -->
 
 
 </section>
+
+
+
 
 
 
@@ -187,7 +190,7 @@
 </script>
 
 
-<div class="  logo-container " style="display:flex ;justify-content: flex-end ; gap: .25rem;">
+<!-- <div class="  logo-container " style="display:flex ;justify-content: flex-end ; gap: .25rem;">
   <a href="https://www.facebook.com/CMOGroupTH/" target="_blank"><img style="width: 22px;height: 22px;" src="<?php echo BASE_URL; ?>assets/images/icon/Logo-FB.png"></img></a>
   <a href="https://www.instagram.com/cmogroupth/" target="_blank"><img style="width: 22px;height: 22px;" src="<?php echo BASE_URL; ?>assets/images/icon/Logo-IG.png"></img></a>
   <a href="https://twitter.com/CMOGroupTH" target="_blank"><img style="width: 22px;height: 22px;" src="<?php echo BASE_URL; ?>assets/images/icon/Logo-Twitter.png"></img></a>
@@ -195,4 +198,4 @@
   <a href="https://www.threads.net/@cmogroupth" target="_blank"><img style="width: 22px;height: 22px;" src="<?php echo BASE_URL; ?>assets/images/icon/Logo-Threads-.png"></img></a>
   <a href="https://page.line.me/798nqsnv" target="_blank"><img style="width: 22px;height: 22px;" src="<?php echo BASE_URL; ?>assets/images/icon/Logo-Line.png"></img></a>
   <a href="https://www.youtube.com/@cmo-group" target="_blank"><img style="width: 22px;height: 22px;" src="<?php echo BASE_URL; ?>assets/images/icon/Logo-Youtube.png"></img></a>
-</div>
+</div> -->
