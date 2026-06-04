951fa4d7d20c3cfbbbd0f1951c98db56476c0a68
Adinan Hayeedaramae <adinance@gmail.com>
2026-06-04 11:36:13 +0700

    Update Management Team.

--- investor-relations/application/views/ir/corporate_info/management_team.php
+++ investor-relations/application/views/ir/corporate_info/management_team.php
@@ -3,8 +3,8 @@
 		<div class="row media-row">
 
 			<div class="team-item col-lg-3 col-md-6">
-				<div class="item-image" id="naphamat">
-					<a href="#management-team-naphamat"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-k-mongkol-510x565.jpg" alt="" title=""></a>
+				<div class="item-image" id="Mongkol">
+					<a href="#management-team-Mongkol"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-k-mongkol-510x565.jpg" alt="" title=""></a>
 				</div>
 
 				<div class="item-caption py-3">
@@ -15,6 +15,7 @@
 					</div>
 					<div class="item-role px-2">
 						<p class="display-4" style="font-size: 0.9em !important; padding-top:10px;">
+							∙ President<br>
 							∙ Chief Executive Officer<br>
 							∙ Chairman of the Sustainability <br>&nbsp; Committee<br>
 							∙ Risk Management Committee <br>&nbsp; Member<br>
@@ -34,26 +35,24 @@
 		<div class="row media-row">
 
 			<div class="team-item col-lg-3 col-md-6">
-				<div class="item-image" id="kitisak">
-					<a href="#management-team-kitisak"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-k-yingrak-510x565.jpg" alt="" title=""></a>
+				<div class="item-image" id="Yingrak">
+					<a href="#management-team-Yingrak"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-k-yingrak-510x565.jpg" alt="" title=""></a>
 				</div>
 				<div class="item-caption py-3">
 					<div class="item-name px-2">
 						<p class="mbr-fonts-style display-4"><strong>Ms. Yingrak Phuathavornskul</strong></p>
 					</div>
 					<div class="item-role px-2">
-						<p class="display-4" style="font-size: 0.9em !important; padding-top:10px;">∙ President
-							<br>∙ Sustainability Committee Member
-							<br>∙ Nomination and Remuneration<br>&nbsp; Director
-							<br>∙ Director
+						<p class="display-4" style="font-size: 0.9em !important; padding-top:10px;">∙ Vice President
+							
 						</p>
 					</div>
 				</div>
 			</div>
 
 			<div class="team-item col-lg-3 col-md-6">
-				<div class="item-image" id="trairong">
-					<a href="#board-of-director-nvin"><img src="<?php echo BASE_URL . PATH_MOBIRISE ; ?>assets/images/board-of-director-k-nvin-420x466.jpg"></a>
+				<div class="item-image" id="Nvin">
+					<a href="#management-team-Nvin"><img src="<?php echo BASE_URL . PATH_MOBIRISE ; ?>assets/images/board-of-director-k-nvin-420x466.jpg"></a>
 				</div>
 				<div class="item-caption py-3">
 					<div class="item-name px-2">
@@ -81,8 +80,8 @@
 			
 			
 			<div class="team-item col-lg-3 col-md-6">
-				<div class="item-image" id="thanaporn">
-					<a href="#management-team-thanaporn"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-k-pichet-510x565.jpg" alt="" title=""></a>
+				<div class="item-image" id="Pichet">
+					<a href="#management-team-Pichet"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-k-pichet-510x565.jpg" alt="" title=""></a>
 				</div>
 				<div class="item-caption py-3">
 					<div class="item-name px-2">
@@ -99,36 +98,34 @@
 				</div>
 			</div>
 			<div class="team-item col-lg-3 col-md-6">
-				<div class="item-image" id="chanasa">
-					<a href="#management-team-chanasa"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-k-vuttiphan-2-510x565.jpg" alt="" title=""></a>
+				<div class="item-image" id="Jutiporn">
+					<a href="#management-team-Jutiporn"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-k-jutiporn-510x565.jpg" alt="" title=""></a>
 				</div>
 				<div class="item-caption py-3">
 					<div class="item-name px-2">
-						<p class="mbr-fonts-style display-4"><strong>Mr. Vuttiphan Tanametanont</strong></p>
+						<p class="mbr-fonts-style display-4"><strong>Ms. Jutiporn Mingkwanrungrueng</strong></p>
 					</div>
 					<div class="item-role px-2">
 						<p class="display-4" style="font-size: 0.9em !important; padding-top:10px;">
-							∙ Chief Strategy and Internal<br>&nbsp;&nbsp;&nbsp;Development Officer<br>
-							∙ Sustainability Committee Member<br>
-							∙ Risk Management Committee <br>&nbsp;&nbsp;Member<br>
-							∙ Director
+							∙ Chief Operating Officer<br>
+							∙ Managing Director
 						</p>
 					</div>
 				</div>
 			</div>
 
 			<div class="team-item col-lg-3 col-md-6">
-				<div class="item-image" id="jutiporn">
-					<a href="#management-team-jutiporn"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-k-mapamas-510x565.jpg" alt="" title=""></a>
+				<div class="item-image" id="Thanaporn">
+					<a href="#management-team-Thanaporn"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-k-thanaporn-510x565.jpg" alt="" title=""></a>
 				</div>
 				<div class="item-caption py-3">
 					<div class="item-name px-2">
-						<p class="mbr-fonts-style display-4"><strong>Ms. Naphamat Phlaingam</strong></p>
+						<p class="mbr-fonts-style display-4"><strong>Ms. Thanaporn Phetluan</strong></p>
 					</div>
 					<div class="item-role px-2">
 						<p class="display-4" style="font-size: 0.9em !important; padding-top:10px;">
-							∙ Chief Financial Officer<br>
-							∙ Director
+							∙ Chief Business Development Officer<br>
+							∙ Managing Director
 						</p>
 					</div>
 				</div>
@@ -143,8 +140,8 @@
 		<div class="row media-row">
 			
 			<div class="team-item col-lg-3 col-md-6">
-				<div class="item-image" id="jutiporn">
-					<a href="#management-team-jutiporn"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-k-mapamas-510x565.jpg" alt="" title=""></a>
+				<div class="item-image" id="Naphamat">
+					<a href="#management-team-Naphamat"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-k-mapamas-510x565.jpg" alt="" title=""></a>
 				</div>
 				<div class="item-caption py-3">
 					<div class="item-name px-2">
@@ -160,8 +157,8 @@
 			</div>
 			
 			<div class="team-item col-lg-3 col-md-6">
-				<div class="item-image" id="chanasa">
-					<a href="#management-team-chanasa"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-k-vuttiphan-2-510x565.jpg" alt="" title=""></a>
+				<div class="item-image" id="Vuttiphan">
+					<a href="#management-team-Vuttiphan"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-k-vuttiphan-2-510x565.jpg" alt="" title=""></a>
 				</div>
 				<div class="item-caption py-3">
 					<div class="item-name px-2">
@@ -181,51 +178,88 @@
 	</div>
 </section>
 
-<section class="cid-upA2kc620b" id="management-team-kitisak">
+
+<section class="cid-upA2kh0pC5" id="management-team-Mongkol">
 	<div class="hovershine">
 		<figure class="mbr-figure">
 			<div class="image-block ">
-				<a href="#kitisak"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-yingrak-1920x1503.jpg" alt="" title=""></a>
+				<a href="#Mongkol"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-mongkol-1920x1886.jpg" alt="" title=""></a>
 			</div>
 		</figure>
 	</div>
 </section>
 
-<section class="cid-upA2kh0pC5" id="management-team-naphamat">
+<section class="cid-upA2kc620b" id="management-team-Yingrak">
 	<div class="hovershine">
 		<figure class="mbr-figure">
 			<div class="image-block ">
-				<a href="#naphamat"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-mongkol-1920x1886.jpg" alt="" title=""></a>
+				<a href="#Yingrak"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-yingrak-1920x1503.jpg" alt="" title=""></a>
 			</div>
 		</figure>
 	</div>
 </section>
 
-<section class="cid-upA2keFHGz" id="management-team-jutiporn">
+
+<section class="cid-upA2kc620b" id="management-team-Nvin">
 	<div class="hovershine">
 		<figure class="mbr-figure">
 			<div class="image-block ">
-				<a href="#mongkol"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/naphamat-1920x2210.jpg" alt="" title=""></a>
+				<a href="#Nvin"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-nvin-en-1920x1805.jpg" alt="" title=""></a>
 			</div>
 		</figure>
 	</div>
 </section>
 
-<section class="cid-upA2kfOeBE" id="management-team-thanaporn">
+
+<section class="cid-upA2kc620b" id="management-team-Pichet">
 	<div class="hovershine">
 		<figure class="mbr-figure">
 			<div class="image-block ">
-				<a href="#pichet"> <img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-pichet-1920x1651.jpg" alt="" title=""></a>
+				<a href="#Pichet"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-pichet-1920x1651.jpg" alt="" title=""></a>
 			</div>
 		</figure>
 	</div>
 </section>
 
-<section class="cid-upA2kks3jt" id="management-team-chanasa">
+
+<section class="cid-upA2kc620b" id="management-team-Jutiporn">
 	<div class="hovershine">
 		<figure class="mbr-figure">
 			<div class="image-block ">
-				<a href="#chanasa"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-vuttiphan-1920x1889.jpg" alt="" title=""></a>
+				<a href="#Jutiporn"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-info-k-jutiporn-en-1920x1370.jpg" alt="" title=""></a>
+			</div>
+		</figure>
+	</div>
+</section>
+
+
+<section class="cid-upA2kfOeBE" id="management-team-Thanaporn">
+	<div class="hovershine">
+		<figure class="mbr-figure">
+			<div class="image-block ">
+				<a href="#Thanaporn"> <img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-info-k-thanaporn-en-1920x1435.jpg" alt="" title=""></a>
+			</div>
+		</figure>
+	</div>
+</section>
+
+
+<section class="cid-upA2keFHGz" id="management-team-Naphamat">
+	<div class="hovershine">
+		<figure class="mbr-figure">
+			<div class="image-block ">
+				<a href="#Naphamat"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/naphamat-1920x2210.jpg" alt="" title=""></a>
+			</div>
+		</figure>
+	</div>
+</section>
+
+
+<section class="cid-upA2kks3jt" id="management-team-Vuttiphan">
+	<div class="hovershine">
+		<figure class="mbr-figure">
+			<div class="image-block ">
+				<a href="#Vuttiphan"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-vuttiphan-1920x1889.jpg" alt="" title=""></a>
 			</div>
 		</figure>
 	</div>
--- investor-relations/application/views/ir/corporate_info/management_team_th.php
+++ investor-relations/application/views/ir/corporate_info/management_team_th.php
@@ -1,167 +1,265 @@
 <section class="team1 cid-upA2k6lyzu" id="management-team">
-    <div class="container align-center">
-        <div class="row media-row">
-        <div class="team-item col-lg-3 col-md-6">
-                <div class="item-image" id="kitisak">
-                    <a href="#management-team-kitisak"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-k-yingrak-510x565.jpg" alt="" title=""></a>
-                </div>
-                <div class="item-caption py-3">
-                    <div class="item-name px-2">
-                        <p class="mbr-fonts-style display-4"><strong>นางสาวยิ่งรัก พัวถาวรสกุล
+	<div class="container align-center">
+		<div class="row media-row">
 
+			<div class="team-item col-lg-3 col-md-6">
+				<div class="item-image" id="Mongkol">
+					<a href="#management-team-Mongkol"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-k-mongkol-510x565.jpg" alt="" title=""></a>
+				</div>
 
+				<div class="item-caption py-3">
+					<div class="item-name px-2">
+						<p class="mbr-fonts-style display-4"><strong>
+								นายมงคล ศีลธรรมพิทักษ์</strong></p>
+					</div>
+					<div class="item-role px-2">
+						<p class="display-4" style="font-size: 0.9em !important; padding-top:10px;">
+							∙ กรรมการผู้จัดการใหญ่<br>
+							∙ ประธานเจ้าหน้าที่บริหาร<br>
+							∙ ประธานกรรมการความยั่งยืน<br>
+							∙ กรรมการบริหารความเสี่ยง<br>
+							∙ กรรมการบริษัท
 
+						</p>
+					</div>
+				</div>
+			</div>
 
-</strong></p>
-                    </div>
-                    <div class="item-role px-2">
-                        <p class="display-4"  style="font-size: 0.9em !important; padding-top:10px;">∙ กรรมการผู้จัดการใหญ่ 
-<br>∙ กรรมการความยั่งยืน
-<br>∙ กรรมการสรรหา และกำหนดค่าตอบแทน
-<br>∙ กรรมการบริษัท
-</p>
-                    </div>
-                </div>
-            </div>
-        </div>    
-    </div>
+		</div>
+	</div>
 </section>
 
 <section class="team1 cid-upA2ka6xys" id="team1-h7">
-    <div class="container align-center">
-        <div class="row media-row">
-        <div class="team-item col-lg-3 col-md-6">
-                <div class="item-image" id="naphamat">
-                <a href="#management-team-naphamat"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-k-mongkol-510x565.jpg" alt="" title=""></a>
-                </div>
-                <div class="item-caption py-3">
-                    <div class="item-name px-2">
-                        <p class="mbr-fonts-style display-4"><strong>
-                           นายมงคล ศีลธรรมพิทักษ์
+	<div class="container align-center">
+		<div class="row media-row">
 
+			<div class="team-item col-lg-3 col-md-6">
+				<div class="item-image" id="Yingrak">
+					<a href="#management-team-Yingrak"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-k-yingrak-510x565.jpg" alt="" title=""></a>
+				</div>
+				<div class="item-caption py-3">
+					<div class="item-name px-2">
+						<p class="mbr-fonts-style display-4"><strong>นางสาวยิ่งรัก พัวถาวรสกุล</strong></p>
+					</div>
+					<div class="item-role px-2">
+						<p class="display-4" style="font-size: 0.9em !important; padding-top:10px;">∙ รองกรรมการผู้จัดการใหญ่ 
+							
+						</p>
+					</div>
+				</div>
+			</div>
 
+			<div class="team-item col-lg-3 col-md-6">
+				<div class="item-image" id="Nvin">
+					<a href="#management-team-Nvin"><img src="<?php echo BASE_URL . PATH_MOBIRISE ; ?>assets/images/board-of-director-k-nvin-420x466.jpg"></a>
+				</div>
+				<div class="item-caption py-3">
+					<div class="item-name px-2">
+						<p class="mbr-fonts-style display-4"><strong>นายนวิน เหมรัชตานันต์</strong></p>
+					</div>
+					<div class="item-role px-2 display-4">
+						<p class="display-4" style="font-size: 0.9em !important; padding-top:10px;">
+							• รองกรรมการผู้จัดการใหญ่
+							<br>• กรรมการความยั่งยืน
+							<br>• กรรมการสรรหาและกำหนดค่าตอบแทน
+							<br>• กรรมการบริษัท
+							<br>• ผู้อำนวยการด้านกลยุทธ์คอนเทนต์
+						</p>
+					</div>
+				</div>
+			</div>
 
-</strong></p>
-                    </div>
-                    <div class="item-role px-2">
-                        <p class="display-4"  style="font-size: 0.9em !important; padding-top:10px;">
-                            ∙ ประธานเจ้าหน้าที่บริหาร<br>
-                            ∙ ประธานกรรมการความยั่งยืน<br>
-                            ∙ กรรมการบริหารความเสี่ยง<br>
-                            ∙ กรรมการบริษัท
+		</div>
+	</div>
+</section>
 
-                        </p>
-                    </div>
-                </div>
-            </div><div class="team-item col-lg-3 col-md-6">
-                <div class="item-image" id="jutiporn">
-                <a href="#management-team-jutiporn"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-k-mapamas-510x565.jpg" alt="" title=""></a>
-                </div>
-                <div class="item-caption py-3">
-                    <div class="item-name px-2">
-                        <p class="mbr-fonts-style display-4"><strong>นางสาวนภามาศ พลายงาม
+<section class="team1 cid-upA2ka6xys" id="team1-h7">
+	<div class="container align-center">
+		<div class="row media-row">
+			
+			
+			<div class="team-item col-lg-3 col-md-6">
+				<div class="item-image" id="Pichet">
+					<a href="#management-team-Pichet"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-k-pichet-510x565.jpg" alt="" title=""></a>
+				</div>
+				<div class="item-caption py-3">
+					<div class="item-name px-2">
+						<p class="mbr-fonts-style display-4"><strong>นายพิเชษฐ ตุรงคินานนท์</strong></p>
+					</div>
+					<div class="item-role px-2 display-4">
+						<p class="display-4" style="font-size: 0.9em !important; padding-top:10px;">
+							∙ ประธานเจ้าหน้าที่บริหารโครงการ<br>
+							∙ กรรมการความยั่งยืน<br>
+							∙ กรรมการบริหารความเสี่ยง<br>
 
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="team-item col-lg-3 col-md-6">
+				<div class="item-image" id="Jutiporn">
+					<a href="#management-team-Jutiporn"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-k-jutiporn-510x565.jpg" alt="" title=""></a>
+				</div>
+				<div class="item-caption py-3">
+					<div class="item-name px-2">
+						<p class="mbr-fonts-style display-4"><strong>นางสาวจุติพร มิ่งขวัญรุ่งเรือง</strong></p>
+					</div>
+					<div class="item-role px-2">
+						<p class="display-4" style="font-size: 0.9em !important; padding-top:10px;">
+							∙ ประธานเจ้าหน้าที่ฝ่ายปฏิบัติการ <br>
+							∙ กรรมการผู้จัดการ 
+						</p>
+					</div>
+				</div>
+			</div>
 
-</strong></p>
-                    </div>
-                    <div class="item-role px-2">
-                        <p class="display-4"  style="font-size: 0.9em !important; padding-top:10px;">
-                            ∙ ประธานเจ้าหน้าที่ฝ่ายการเงิน<br>
-                            ∙ กรรมการบริษัท
-                    </p>
-                    </div>
-                </div>
-            </div><div class="team-item col-lg-3 col-md-6">
-                <div class="item-image" id="thanaporn">
-                <a href="#management-team-thanaporn"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-k-pichet-510x565.jpg" alt="" title=""></a>
-                </div>
-                <div class="item-caption py-3">
-                    <div class="item-name px-2">
-                        <p class="mbr-fonts-style display-4"><strong>นายพิเชษฐ ตุรงคินานนท์
-
-
-</strong></p>
-                    </div>
-                    <div class="item-role px-2 display-4">
-                        <p class="display-4"  style="font-size: 0.9em !important; padding-top:10px;">
-                            ∙ ประธานเจ้าหน้าที่บริหารโครงการ<br>
-                            ∙ กรรมการความยั่งยืน<br>
-                            ∙ กรรมการบริหารความเสี่ยง<br>
-                            
-                    </p>
-                    </div>
-                </div>
-            </div><div class="team-item col-lg-3 col-md-6">
-                <div class="item-image" id="chanasa">
-                <a href="#management-team-chanasa"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-k-vuttiphan-2-510x565.jpg" alt="" title=""></a>
-                </div>
-                <div class="item-caption py-3">
-                    <div class="item-name px-2">
-                        <p class="mbr-fonts-style display-4"><strong>นายวุฒิพันธ์ ธนะเมธานนท์ 
-</strong></p>
-                    </div>
-                    <div class="item-role px-2">
-                        <p class="display-4"  style="font-size: 0.9em !important; padding-top:10px;">
-                            ∙ ประธานเจ้าหน้าที่ฝ่ายกลยุทธ์ <br>&nbsp;&nbsp;และพัฒนาภายใน<br>
-                            ∙ กรรมการความยั่งยืน<br>
-                            ∙ กรรมการบริหารความเสี่ยง<br>
-                            ∙ กรรมการบริษัท
-                        </p>
-                    </div>
-                </div>
-            </div></div>    
-    </div>
+			<div class="team-item col-lg-3 col-md-6">
+				<div class="item-image" id="Thanaporn">
+					<a href="#management-team-Thanaporn"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-k-thanaporn-510x565.jpg" alt="" title=""></a>
+				</div>
+				<div class="item-caption py-3">
+					<div class="item-name px-2">
+						<p class="mbr-fonts-style display-4"><strong>นางสาวธนภร เพ็ชรล้วน</strong></p>
+					</div>
+					<div class="item-role px-2">
+						<p class="display-4" style="font-size: 0.9em !important; padding-top:10px;">
+							∙ ประธานเจ้าหน้าที่ฝ่ายพัฒนาธุรกิจ<br>
+							∙ กรรมการผู้จัดการ
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
 
 
-<section class="cid-upA2kc620b" id="management-team-kitisak">
-<div class="hovershine">
-    <figure class="mbr-figure">
-        <div class="image-block " >
-        <a href="#kitisak"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-yingrak-th-1920x1556.jpg"  alt="" title=""></a>
-        </div>
-    </figure>
-</div>
+<section class="team1 cid-upA2ka6xys" id="team1-h7">
+	<div class="container align-center">
+		<div class="row media-row">
+			
+			<div class="team-item col-lg-3 col-md-6">
+				<div class="item-image" id="Naphamat">
+					<a href="#management-team-Naphamat"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-k-mapamas-510x565.jpg" alt="" title=""></a>
+				</div>
+				<div class="item-caption py-3">
+					<div class="item-name px-2">
+						<p class="mbr-fonts-style display-4"><strong>นางสาวนภามาศ พลายงาม</strong></p>
+					</div>
+					<div class="item-role px-2">
+						<p class="display-4" style="font-size: 0.9em !important; padding-top:10px;">
+							∙ ประธานเจ้าหน้าที่ฝ่ายการเงิน<br>
+							∙ กรรมการบริษัท
+						</p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="team-item col-lg-3 col-md-6">
+				<div class="item-image" id="Vuttiphan">
+					<a href="#management-team-Vuttiphan"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-k-vuttiphan-2-510x565.jpg" alt="" title=""></a>
+				</div>
+				<div class="item-caption py-3">
+					<div class="item-name px-2">
+						<p class="mbr-fonts-style display-4"><strong>นายวุฒิพันธ์ ธนะเมธานนท์ </strong></p>
+					</div>
+					<div class="item-role px-2">
+						<p class="display-4" style="font-size: 0.9em !important; padding-top:10px;">
+							∙ ประธานเจ้าหน้าที่ฝ่ายกลยุทธ์ และพัฒนาภายใน<br>
+							∙ กรรมการความยั่งยืน<br>
+							∙ กรรมการบริหารความเสี่ยง<br>
+							∙ กรรมการบริษัท
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
 
-<section class="cid-upA2kh0pC5" id="management-team-naphamat">
-<div class="hovershine">
-    <figure class="mbr-figure">
-        <div class="image-block " >
-        <a href="#naphamat"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-mongkol-th-1920x1813.jpg"  alt="" title=""></a>
-        </div>
-    </figure>
-</div>
+
+<section class="cid-upA2kh0pC5" id="management-team-Mongkol">
+	<div class="hovershine">
+		<figure class="mbr-figure">
+			<div class="image-block ">
+				<a href="#Mongkol"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-mongkol-th-1920x1813.jpg" alt="" title=""></a>
+			</div>
+		</figure>
+	</div>
 </section>
 
-<section class="cid-upA2keFHGz" id="management-team-jutiporn">
-<div class="hovershine">
-    <figure class="mbr-figure">
-        <div class="image-block " >
-        <a href="#mongkol"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/naphamat-th-1920x2139.jpg"  alt="" title=""></a>
-        </div>
-    </figure>
-</div>
-</section>
-
-<section class="cid-upA2kfOeBE" id="management-team-thanaporn">
-<div class="hovershine">
-    <figure class="mbr-figure">
-        <div class="image-block " >
-           <a href="#pichet"> <img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-pichet-th-1920x1622.jpg"  alt="" title=""></a>
-        </div>
-    </figure>
-</div>
+<section class="cid-upA2kc620b" id="management-team-Yingrak">
+	<div class="hovershine">
+		<figure class="mbr-figure">
+			<div class="image-block ">
+				<a href="#Yingrak"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-yingrak-th-1920x1556.jpg" alt="" title=""></a>
+			</div>
+		</figure>
+	</div>
 </section>
 
 
-<section class="cid-upA2kks3jt" id="management-team-chanasa">
-<div class="hovershine">
-    <figure class="mbr-figure">
-        <div class="image-block " >
-        <a href="#chanasa"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-vuttiphan-th-1920x1850.jpg"  alt="" title=""></a>
-        </div>
-    </figure>
-</div>
+<section class="cid-upA2kc620b" id="management-team-Nvin">
+	<div class="hovershine">
+		<figure class="mbr-figure">
+			<div class="image-block ">
+				<a href="#Nvin"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-nvin-th-1920x1902.jpg" alt="" title=""></a>
+			</div>
+		</figure>
+	</div>
 </section>
 
+
+<section class="cid-upA2kc620b" id="management-team-Pichet">
+	<div class="hovershine">
+		<figure class="mbr-figure">
+			<div class="image-block ">
+				<a href="#Pichet"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-pichet-th-1920x1622.jpg" alt="" title=""></a>
+			</div>
+		</figure>
+	</div>
+</section>
+
+
+<section class="cid-upA2kc620b" id="management-team-Jutiporn">
+	<div class="hovershine">
+		<figure class="mbr-figure">
+			<div class="image-block ">
+				<a href="#Jutiporn"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-info-k-jutiporn-th-1920x1362.jpg" alt="" title=""></a>
+			</div>
+		</figure>
+	</div>
+</section>
+
+
+<section class="cid-upA2kfOeBE" id="management-team-Thanaporn">
+	<div class="hovershine">
+		<figure class="mbr-figure">
+			<div class="image-block ">
+				<a href="#Thanaporn"> <img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/management-team-info-k-thanaporn-th-1920x1454.jpg" alt="" title=""></a>
+			</div>
+		</figure>
+	</div>
+</section>
+
+
+<section class="cid-upA2keFHGz" id="management-team-Naphamat">
+	<div class="hovershine">
+		<figure class="mbr-figure">
+			<div class="image-block ">
+				<a href="#Naphamat"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/naphamat-th-1920x2139.jpg" alt="" title=""></a>
+			</div>
+		</figure>
+	</div>
+</section>
+
+
+<section class="cid-upA2kks3jt" id="management-team-Vuttiphan">
+	<div class="hovershine">
+		<figure class="mbr-figure">
+			<div class="image-block ">
+				<a href="#Vuttiphan"><img src="<?php echo BASE_URL . PATH_MOBIRISE; ?>assets/images/board-of-director-info-k-vuttiphan-th-1920x1850.jpg" alt="" title=""></a>
+			</div>
+		</figure>
+	</div>
+</section>
